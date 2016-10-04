5936cda4444f01cf3780c6ecae06113d57b53ceb
Nobuhiko Okubo <nobuhiko_okubo@unirita.co.jp>
2016-10-05 08:49:13 +0900

    fixed Q1-Main

--- 第2回/Q1/Main.java
+++ 第2回/Q1/Main.java
@@ -6,11 +6,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String line = br.readLine();
+        br.close();
+
         StringBuilder sb = new StringBuilder();
-        for(int i = line.length(); i > 0; i--) {
-            sb.append(line.substring(i - 1, i));
+        for(int i = line.length() - 1; i >= 0; i--) {
+            sb.append(line.charAt(i));
         }
         System.out.println(sb.toString());
-        br.close();
     }
 }
