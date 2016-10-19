ea9bc22b468ef1898b5380eb7d8142b2aa52852f
Nobuhiko Okubo <nobuhiko_okubo@unirita.co.jp>
2016-10-20 08:53:53 +0900

    fixed Q2

--- 第2回/Q2/Main.java
+++ 第2回/Q2/Main.java
@@ -1,82 +1,73 @@
 ﻿import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
+    
+    static final String CONVERT_TABLE = "A234567890JQK";
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String line = br.readLine();
         String[] card = line.split(" ");
-        int[] spead = new int[card.length];
-        int[] dia   = new int[card.length];
-        int[] club  = new int[card.length];
-        int[] heart = new int[card.length];
-        int s = 0, d = 0, c = 0, h = 0;
+        
+        ArrayList<Integer> sList = new ArrayList<Integer>(); //スペードの数値のリスト
+        ArrayList<Integer> dList = new ArrayList<Integer>(); //ダイアの数値のリスト
+        ArrayList<Integer> cList = new ArrayList<Integer>(); //クラブの数値のリスト
+        ArrayList<Integer> hList = new ArrayList<Integer>(); //ハートの数値のリスト
 
         for(int i = 0; i < card.length; i++) {
-            String mark   = card[i].substring(0, 1);
-            String number = card[i].substring(1, 2);
-            int convertNumber = 0;
-
-            if(number.equals("A")) {
-                convertNumber = 1;
-            } else if(number.equals("0")) {
-                convertNumber = 10;
-            } else if(number.equals("J")) {
-                convertNumber = 11;
-            } else if(number.equals("Q")) {
-                convertNumber = 12;
-            } else if(number.equals("K")) {
-                convertNumber = 13;
-            } else {
-                convertNumber = Integer.parseInt(number);
-            }
-
-            if(mark.equals("S")) spead[s++] = convertNumber;
-            if(mark.equals("D")) dia[d++]   = convertNumber;
-            if(mark.equals("C")) club[c++]  = convertNumber;
-            if(mark.equals("H")) heart[h++] = convertNumber;
+            char mark = card[i].charAt(0);
+            char number = card[i].charAt(1);
+            int convertNumber = convertCharNumberToInt(number);
+            
+            if(mark == 'S') sList.add(convertNumber);
+            if(mark == 'D') dList.add(convertNumber);
+            if(mark == 'C') cList.add(convertNumber);
+            if(mark == 'H') hList.add(convertNumber);
         }
-        Arrays.sort(spead);
-        Arrays.sort(dia);
-        Arrays.sort(club);
-        Arrays.sort(heart);
 
-        printCardNumber(spead, s, "S");
-        printCardNumber(dia, d, "D");
-        printCardNumber(club, c, "C");
-        printCardNumber(heart, h, "H");
+        Collections.sort(sList);
+        Collections.sort(dList);
+        Collections.sort(cList);
+        Collections.sort(hList);
+
+        printCardNumber(sList, 'S');
+        printCardNumber(dList, 'D');
+        printCardNumber(cList, 'C');
+        printCardNumber(hList, 'H');
+    }
+    
+    static int convertCharNumberToInt(char number) {
+        for(int i = 1; i <= 13; i++) {
+            if(CONVERT_TABLE.charAt(i - 1) == number) return i;
+        }
+        return 0;
     }
 
-    static void printCardNumber(int[] markArray, int markCount, String mark) {
-        if(markCount == 0) return;
+    static char convertIntNumberToChar(int number) {
+        return CONVERT_TABLE.charAt(number - 1);
+    }
+    
+    static void printCardNumber(List<Integer> markList, char mark) {
+        if(markList.size() == 0) return;
 
-        int number = 0;
-        String outputNumber = "";
         System.out.print(mark + ":");
-        for(int i = 0; i < markArray.length; i++) {
-            number = markArray[i];
-            if(number == 0) {
-                continue;
-            } else if(number == 1) {
-                outputNumber = "A";
-            } else if(number == 10) {
-                outputNumber = "0";
-            } else if(number == 11) {
-                outputNumber = "J";
-            } else if(number == 12) {
-                outputNumber = "Q";
-            } else if(number == 13) {
-                outputNumber = "K";
-            } else {
-                outputNumber = String.valueOf(number);
-            }
+        
+        int number;
+        char convertNumber;
+        
+        for(int i = 0; i < markList.size(); i++) {
+            number = markList.get(i);
+            convertNumber = convertIntNumberToChar(number);
 
-            if(i == markArray.length - 1) {
-                System.out.println(outputNumber);
+            if(i == markList.size() - 1) {
+                System.out.println(convertNumber);
             } else {
-                System.out.print(outputNumber + ",");
+                System.out.print(convertNumber + ',');
             }
         }
     }
