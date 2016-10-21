8a5681c42e9ee1429eb6e361ec3d787793af799e
Nobuhiko Okubo <nobuhiko_okubo@unirita.co.jp>
2016-10-21 12:37:39 +0900

    fixed Q3

--- 第2回/Q3/Main.java
+++ 第2回/Q3/Main.java
@@ -1,23 +1,22 @@
 ﻿import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String line = br.readLine();
-        List<String> rgbList = new ArrayList<String>();
-        String rgb = "";
-        String left = "";
-        String right = "";
-        String afterRgb = "";
+        LinkedList<Character> rgbList = new LinkedList<Character>();
+        char rgb;
+        char left;
+        char right;
+        char afterRgb;
 
         for(int i = 0; i < line.length(); i++) {
-            rgb = line.substring(i, i + 1);
+            rgb = line.charAt(i);
             if(rgbList.size() == 0) {
-                rgbList.add(rgb);
+                rgbList.addLast(rgb);
                 continue;
             }
 
@@ -25,29 +24,29 @@ public class Main {
             right = rgbList.get(rgbList.size() - 1);
 
             if(rgbList.size() == 1) {
-                if(rgb.equals(rgbList.get(0))) {
-                    rgbList.remove(rgbList.size() - 1);
+                if(rgb == rgbList.get(0)) {
+                    rgbList.remove(0);
                 } else {
-                    rgbList.add(rgb);
+                    rgbList.addLast(rgb);
                 }
                 continue;
             }
 
-            if(left.equals(rgb)) {
+            if(left == rgb) {
                 rgbList.remove(0);
-            } else if(right.equals(rgb)) {
+            } else if(right == rgb) {
                 rgbList.remove(rgbList.size() - 1);
             } else if(i == line.length() - 1)  {
-                rgbList.add(rgb);
+                rgbList.addLast(rgb);
                 continue;
             } else {
-                afterRgb = line.substring(i + 1, i + 2);
-                if(left.equals(afterRgb)) {
-                    rgbList.add(rgb);
-                } else if(right.equals(afterRgb)) {
-                    rgbList.add(0, rgb);
+                afterRgb = line.charAt(i + 1);
+                if(left == afterRgb) {
+                    rgbList.addLast(rgb);
+                } else if(right == afterRgb) {
+                    rgbList.addFirst(rgb);
                 } else {
-                    rgbList.add(rgb);
+                    rgbList.addLast(rgb);
                 }
             }
         }
